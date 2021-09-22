e1309ea4951141f18a525667b2bcb761f6d98ffe
Igor Mircic <igor.mircic@live.com>
2021-09-22 20:04:10 +0200

    finish part1 courseinfo exercises

---
+++ part1/courseinfo/src/components/Part.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const Part = ({ part, exercises }) => {
+  return (
+    <div>
+      <p>
+        {part} {exercises}
+      </p>
+    </div>
+  );
+};
---
+++ part1/courseinfo/src/components/Total.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Total = ({ exercises1, exercises2, exercises3 }) => {
+  return (
+    <div>
+      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+    </div>
+  );
+};
