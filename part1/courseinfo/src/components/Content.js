abeeb219d88189d3e98083c6e20839f818c98350
Igor Mircic <igor.mircic@live.com>
2021-09-23 02:08:16 +0200

    finish step 4 for courseinfo

--- part1/courseinfo/src/components/Content.js
+++ part1/courseinfo/src/components/Content.js
@@ -1,12 +1,12 @@
 import React from "react";
 import { Part } from "./Part";
 
-export const Content = ({ part1, part2, part3 }) => {
+export const Content = ({ parts }) => {
   return (
     <div>
-      <Part part={part1.name} exercises={part1.exercises} />
-      <Part part={part2.name} exercises={part2.exercises} />
-      <Part part={part3.name} exercises={part3.exercises} />
+      {parts.map((p) => (
+        <Part part={p.name} exercises={p.exercises} />
+      ))}
     </div>
   );
 };
--- part1/courseinfo/src/components/Total.js
+++ part1/courseinfo/src/components/Total.js
@@ -1,9 +1,10 @@
 import React from "react";
 
-export const Total = ({ exercises1, exercises2, exercises3 }) => {
+export const Total = ({ parts }) => {
+  const total = parts.reduce((p, c) => p + c.exercises, 0);
   return (
     <div>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+      <p>Number of exercises {total}</p>
     </div>
   );
 };
